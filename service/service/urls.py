751d40a25106e136051c6e93f9711eb3731f7b2d
Merk9429 <connor9429@gmail.com>
2023-09-16 18:00:42 +0700

    16.09.2029 18:00

--- service/service/urls.py
+++ service/service/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('swagger-ui/', TemplateView.as_view(
+    path('swagger/', TemplateView.as_view(
         template_name='swagger.html',
         extra_context={'schema_url': 'openapi-schema'}
     ), name='swagger'),
