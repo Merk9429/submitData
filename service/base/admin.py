461fe64ef50abef25fb308d5052280f3e6b05cc7
Merk9429 <connor9429@gmail.com>
2023-10-19 21:44:33 +0700

    Все проекты

--- service/base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Author)
-admin.site.register(Coords)
-admin.site.register(Images)
-admin.site.register(Level)
-admin.site.register(Pereval)
